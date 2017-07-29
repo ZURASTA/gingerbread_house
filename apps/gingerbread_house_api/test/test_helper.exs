dbe2fa280086601d72492ac5683ba33da4a6e1db
Stefan Johnson <sj@scrimpycat.io>
2017-07-30 01:40:15 +1000

    Fix up tests that depend on the service

--- apps/gingerbread_house_api/test/test_helper.exs
+++ apps/gingerbread_house_api/test/test_helper.exs
@@ -1,1 +1,6 @@
+Application.ensure_all_started(:gingerbread_house_service)
+Application.ensure_all_started(:ecto)
+
 ExUnit.start()
+
+Ecto.Adapters.SQL.Sandbox.mode(GingerbreadHouse.Service.Repo, :manual)
