6ecadb96b50e1e6dd638cdcb0c9b832ed354a24d
Stefan Johnson <sj@scrimpycat.io>
2017-09-20 00:19:38 +1000

    Document application

--- apps/gingerbread_house_service/lib/gingerbread_house.service.ex
+++ apps/gingerbread_house_service/lib/gingerbread_house.service.ex
@@ -1,8 +1,21 @@
 defmodule GingerbreadHouse.Service do
-    @moduledoc false
+    @moduledoc """
+      The service application for managing and verifying business
+      details.
+    """
 
     use Application
 
+    @doc """
+      Start the service application.
+
+      By default the service will run in `:auto` setup mode, which
+      will automatically create and migrate the database. To override
+      this behaviour, the mode can be passed in as an argument for the
+      `:setup_mode` key. The supported setup modes are: `:auto` and
+      `:manual`. If using manual mode, the state of the database is
+      left up to the caller.
+    """
     def start(_type, args) do
         import Supervisor.Spec, warn: false
 
