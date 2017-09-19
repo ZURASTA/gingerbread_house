59ed0d663253b175ac420dd44ca2c0edc76d4563
Stefan Johnson <sj@scrimpycat.io>
2017-09-19 23:58:38 +1000

    Create Ecto type based on adapter

--- apps/gingerbread_house_service/lib/gingerbread_house.service/business/type_enum.ex
+++ apps/gingerbread_house_service/lib/gingerbread_house.service/business/type_enum.ex
@@ -1,3 +1,8 @@
 import EctoEnum
 
-defenum GingerbreadHouse.Service.Business.TypeEnum, :business_type, [:individual, :company]
+if Application.get_env(:gingerbread_house_service, GingerbreadHouse.Service.Repo, adapter: Ecto.Adapters.Postgres)[:adapter] == Ecto.Adapters.Postgres do
+    quote do: defenum GingerbreadHouse.Service.Business.TypeEnum, :business_type, [:individual, :company]
+else
+    quote do: defenum GingerbreadHouse.Service.Business.TypeEnum, [individual: 0, company: 1]
+end
+|> Code.eval_quoted()
